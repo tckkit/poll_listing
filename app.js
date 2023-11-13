c7bae497f50e4a9c85386c29aa833b168561d7f3
Jason Tang <jasontang14@hotmail.com>
2023-11-13 22:41:42 +0800

    add: app use cors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { pollRouter } from "./src/route/index.js";
+import cors from "cors";
 
 const app = express();
 const PORT = 8080;
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/", pollRouter);
